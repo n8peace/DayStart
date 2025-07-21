518accdf2da69b6fa240eff0cc230f793b150253
n8peace <n8peace@gmail.com>
2025-07-20 18:43:01 -0700

    Update generate-script function timestamp to include time - testing new GitHub Actions V2 workflow

--- supabase/functions/generate-script/index.ts
+++ supabase/functions/generate-script/index.ts
@@ -1,4 +1,4 @@
-// Last Updated: 2024-07-20
+// Last Updated: 2024-07-20 14:30:00
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 import { ContentBlock } from '../shared/types.ts'
